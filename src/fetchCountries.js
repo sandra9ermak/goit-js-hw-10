b5757a598ad1bc61da3e32e6551f50d0831e03c2
sandra9ermak <sandra1ermak@gmail.com>
2021-10-07 18:38:55 +0300

    fin

---
+++ src/fetchCountries.js
@@ -0,0 +1,38 @@
+export const countryList = document.querySelector('.country-list');
+export const countryDiv = document.querySelector('.country-info');
+
+export const fetchCountriesFunc = (name) => new Promise((res, rej) => {
+    return fetch(`https://restcountries.com/v2/name/${name}`)
+        .then(response => { 
+            if (response.status >= 200 && response.status < 300) {
+                return response.json();
+            } 
+            rej('error request'); 
+        }) 
+        .then(data => res(data)); 
+});
+
+export const dataCountriesList = (country => {
+    countryDiv.innerHTML = '';
+    const markup = country.map(item => {
+        return `<li class = "dataCountriesList">
+                <img src="${item.flag}" alt="${item.name}" width="50" height="50">
+                <h3 class = "dataCountriesListImg">${item.name}</h3>
+                </li>`;
+        }).join('');
+
+    countryList.innerHTML = markup;
+});
+
+export const dataCountriesDiv = (country => {
+    countryList.innerHTML = '';
+    const markup = country.map(item => {
+        return `<img src="${item.flag}" alt="${item.name}" width="220" height="110">
+                <h2>${item.name}</h2>
+                <p>Capital: <span class = "dataCountriesDivValue">${item.capital}</span></p>
+                <p>Population: <span class = "dataCountriesDivValue">${item.population}</span></p>
+                <p>Languages: <span class = "dataCountriesDivValue">${item.languages.map(item => item.name)}</span></p>`
+        }).join('');
+
+    countryDiv.innerHTML = markup;
+});
--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 import './css/styles.css';
+import Notiflix from 'notiflix';
+import debounce from 'lodash.debounce';
+import { fetchCountriesFunc, countryList, countryDiv, dataCountriesList, dataCountriesDiv } from './fetchCountries';
 
-const DEBOUNCE_DELAY = 300;
+const input = document.querySelector('input#search-box');
+
+input.addEventListener('input', debounce(event => {
+    if (!input.value.trim()) {
+        countryList.innerHTML = '';
+        countryDiv.innerHTML = '';
+        return false;
+    }
+    fetchCountriesFunc(input.value)
+        .then((country) => {
+            if (country.length > 2 && country.length <= 10) {
+                return dataCountriesList(country);
+            } else if (country.length === 1) {
+                return dataCountriesDiv(country);
+            } else {
+                return Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+            }
+        })
+        .catch((error) => Notiflix.Notify.failure('Oops, there is no country with that name'));
+}, 300));
+
